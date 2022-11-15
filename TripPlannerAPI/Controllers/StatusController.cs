7eabc0829df1a902b801e6b0a67e3a9b107e6782
hipmonkey11 <hipmonkey.11@gmail.com>
2022-11-16 00:17:35 +0530

    First commit
    First commit

---
+++ TripPlannerAPI/Controllers/StatusController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlySmoother.Api.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public string GetRootStatus()
+        {
+            return "I am healthy - TripPlanner";
+        }
+    }
+}
